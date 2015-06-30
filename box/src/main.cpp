448d4e11ebc6d0e42a307be6ee66f216e97ff3be
hoosierEE <ashroyer@gmail.com>
2015-06-29 23:49:30 -0400

    stub out timer

--- box/src/main.cpp
+++ box/src/main.cpp
@@ -15,6 +15,7 @@ SimpleSwitch s2(2);
 SimpleSwitch s3(3);
 
 // MODELS
+uint32_t t0;
 SimpleSwitch switches[NUM_SWITCHES]{s0,s1,s2,s3};
 const char letters[NUM_SWITCHES]{'a','b','c','d'}; // friendly names for buttons
 struct KeyState {
@@ -22,7 +23,18 @@ struct KeyState {
     bool prev[NUM_SWITCHES];
     bool changed;
 };
-struct KeyState key;
+struct KeyState ks;
+
+void resetTimer(void)
+{
+    // TODO this thing
+    if (Serial.available()) {
+        char inbyte = Serial.read();
+        if (inbyte == '0') {
+            t0 = 0;
+        }
+    }
+}
 
 void serialize(uint32_t now, struct KeyState *state)
 {
@@ -33,7 +45,7 @@ void serialize(uint32_t now, struct KeyState *state)
     }
     Serial.print(result, HEX); // '0' through 'F'
     Serial.print(" "); // easier for human to read
-    Serial.println(now);
+    Serial.println(now, HEX); // hex is about 12% faster on average
 }
 
 // RENDER
@@ -47,20 +59,20 @@ void loop() {
     uint32_t now = micros();
     for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
         switches[i].update(now);
-        key.keys[i] = switches[i].pressed();
-        if (key.prev[i] != key.keys[i]) {
+        ks.keys[i] = switches[i].pressed();
+        if (ks.prev[i] != ks.keys[i]) {
             Keyboard.write(letters[i]); // do this the instant they arrive
-            key.changed = true;
+            ks.changed = true;
         }
     }
 
     // if there were any changes, send an update
-    if (key.changed) {
-        serialize(now, &key); // send these in a batch
+    if (ks.changed) {
+        serialize(now, &ks); // send these in a batch
         for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
-            key.prev[i] = key.keys[i]; // make previous same as current
+            ks.prev[i] = ks.keys[i]; // make previous same as current
         }
-        key.changed = false; // reset so we don't send the same update twice
+        ks.changed = false; // reset so we don't send the same update twice
     }
 }
 
