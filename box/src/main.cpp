19a07a98d606a88688fec2bddafc4744d808642c
hoosierEE <ashroyer@gmail.com>
2015-06-29 23:08:39 -0400

    add scons file to gitignore

--- box/src/main.cpp
+++ box/src/main.cpp
@@ -8,18 +8,18 @@
 #include "../lib/SimpleSwitch/SimpleSwitch.h"
 
 // HARDWARE
-const uint8_t NumSwitches{4};
+const uint8_t NUM_SWITCHES{4};
 SimpleSwitch s0(0);
 SimpleSwitch s1(1);
 SimpleSwitch s2(2);
 SimpleSwitch s3(3);
 
 // MODELS
-SimpleSwitch switches[NumSwitches]{s0,s1,s2,s3};
-const char letters[NumSwitches]{'a','b','c','d'}; // friendly names for buttons
+SimpleSwitch switches[NUM_SWITCHES]{s0,s1,s2,s3};
+const char letters[NUM_SWITCHES]{'a','b','c','d'}; // friendly names for buttons
 struct KeyState {
-    bool keys[NumSwitches];
-    bool prev[NumSwitches];
+    bool keys[NUM_SWITCHES];
+    bool prev[NUM_SWITCHES];
     bool changed;
 };
 struct KeyState key;
@@ -28,7 +28,7 @@ void serialize(uint32_t now, struct KeyState *state)
 {
     // Send all the buttons (in hex) and a timestamp (in decimal)
     char result{0};
-    for (uint8_t i = 0; i < NumSwitches; ++i) {
+    for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
         result |= state->keys[i] << i;
     }
     Serial.print(result, HEX); // '0' through 'F'
@@ -45,7 +45,7 @@ void setup() {
 void loop() {
     // UPDATE
     uint32_t now = micros();
-    for (uint8_t i = 0; i < NumSwitches; ++i) {
+    for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
         switches[i].update(now);
         key.keys[i] = switches[i].pressed();
         if (key.prev[i] != key.keys[i]) {
@@ -57,7 +57,7 @@ void loop() {
     // if there were any changes, send an update
     if (key.changed) {
         serialize(now, &key); // send these in a batch
-        for (uint8_t i = 0; i < NumSwitches; ++i) {
+        for (uint8_t i = 0; i < NUM_SWITCHES; ++i) {
             key.prev[i] = key.keys[i]; // make previous same as current
         }
         key.changed = false; // reset so we don't send the same update twice
--- box/src/src.ino
+++ box/src/src.ino
@@ -1,9 +1,2 @@
-/*
- * This space intentionally left blank.
- *
- * If you try to compile this using the Arduino IDE, you probably will have to #include
- * things here such as Wire.h and EEPROM.h but possibly not.  I'm using PlatformIO
- * and targeting Teensy and so far have not needed to #include anything extra whatsoever.
- *
- * See: http://forum.arduino.cc/index.php?topic=271864.0
- */
+// This space intentionally left blank.
+// http://forum.arduino.cc/index.php?topic=271864.0
