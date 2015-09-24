9175222afec0611f994adcb7838ae9537a41ed3f
hoosierEE <ashroyer@gmail.com>
2015-09-24 16:08:37 -0400

    more pseudocode, some actual code

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,21 +1,18 @@
-// Firmware for TeensyLC, built with PlatformIO.
+// Response button firmware for TeensyLC, built with PlatformIO.
 //
-// Implements a USB response box with 1ms timing resolution.
-// To ensure 1ms resolution, latency information is part of 
-// the transmitted data.
-//
-// The other data can be whatever sort of responses you need,
-// in this case it's a set of pushbuttons.
+// Latency is encoded with each data transmission, to be able
+// to time-align different data sources. If each connected device
+// follows this protocol, a network of devices can operate without
+// a common clock, but produce synchronized timing data.
 #include <Arduino.h>
 #include <Bounce.h>
 
-// CONSTANTS
-const uint8_t NUM_BUTTONS{4};
-const uint8_t DEBOUNCE_TIME{100};
+// MODEL
+elapsedMicros loop_timer;
+const uint9_t NUM_BUTTONS{4};
+uint8_t button_pins[NUM_BUTTONS] = { 2, 3, 4, 5 }; // Teensy digital pins
 
-// GLOBAL OBJECTS
-elapsedMicros timer;
-uint8_t button_pins[NUM_BUTTONS] = { 2, 3, 4, 5 };
+const uint8_t DEBOUNCE_TIME{100}; // ms
 Bounce buttons[NUM_BUTTONS] =
 {
     Bounce(button_pins[0],DEBOUNCE_TIME),
@@ -24,17 +21,18 @@ Bounce buttons[NUM_BUTTONS] =
     Bounce(button_pins[3],DEBOUNCE_TIME)
 };
 
+// FUNCTIONS
+
 // PROGRAM
-void setup(){
+void setup() {
     for (uint8_t i = 0; i < NUM_BUTTONS; ++i) {
         // initialize buttons
         pinMode(button_pins[i], INPUT_PULLUP);
     }
 }
 
-void loop(){
+void loop() {
     // from http://alexshroyer.com/Synchronized-Devices/
-    //
     // 1. Accept 'begin' signal (and system time) from host PC.
     // 2. Respond by setting uC clock to 0.
     // 3. Emit data with time == 0 + delta_time.
@@ -42,9 +40,16 @@ void loop(){
     // 5. Subsequent data includes round-trip latency.
 
     // UPDATE
-    // gather input data at short intervals
+    // Gather input data as fast as possible.
+    // input sources include buttons, Serial, etc.
+
     
     // RENDER
-    // send output data at longer intervals
+    // Send output data at a controlled rate.
+    const uint16_t LOOP_TIME{10000}; // us
+    if (timer >= LOOP_TIME) {
+        timer -= LOOP_TIME;
+        // output data now
+    }
 }
 
