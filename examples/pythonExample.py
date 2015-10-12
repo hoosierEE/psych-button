1cc126f241d9087b107b4f104afbf0598875ff78
hoosierEE <ashroyer@gmail.com>
2015-10-12 10:21:22 -0400

    add some comments

--- examples/pythonExample.py
+++ examples/pythonExample.py
@@ -1,15 +1,18 @@
-import serial
 import time
+import serial
 from serial.tools import list_ports
 
-# TODO: change usbmodem45 to whatever your system shows when you enter
-coms = serial.tools.list_ports.comports()
+# Something funky going on here. 
+# Sometimes it works.  Other times it leaves behind the previous
+# `likelyPort` and attempts to open it again after it's been closed.
+coms = list_ports.comports()
 likelyPort = coms[['usbmodem' in x[0] for x in coms].index(True)][0]
-
 print "Trying port ", likelyPort
 
-ser = serial.Serial(likelyPort, 9600, timeout=1)#or something like this for Mac/Linux machines
+# open the chosen serial port
+ser = serial.Serial(likelyPort, 9600, timeout=1) #or something like this for Mac/Linux machines
 
+# Timing test
 t1 = []
 t2 = []
 amt = 10
@@ -19,10 +22,8 @@ for x in range(0,amt):
     line = ser.readline()   # read a '\n' terminated line
     t2.append(time.clock())
 
-print "30x t1: "
-print t1
-print "30x t2: "
-print t2
+print amt, " times t1: " t1
+print amt, " times t2: " t2
 print (sum(t2) - sum(t1))
 
 ser.close()
